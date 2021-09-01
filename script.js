3464b14a2b3ddd88f82b966238689fd84cca8109
wallanmota <wallanmota.13@gmail.com>
2021-09-01 16:14:16 -0300

    tocando som da tecla digitada

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.body.addEventListener('keyup', (event) => {
+document.body.addEventListener('keydown', (event) => {
     playSound(event.code.toLowerCase());
 });
 
 function playSound(sound){
+    let audioElement = document.querySelector(`#s_${sound}`);
 
+    audioElement ? audioElement.play() : console.log("tecla nao encontrada")
 };
